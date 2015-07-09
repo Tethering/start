d4815885d97d6c910ecdb49deb1108b6267174cb
imNavi <procenkoeg@mail.ru>
2015-07-09 06:51:33 +0300

    work in progress

--- Game/scripts/vscripts/addon_game_mode.lua
+++ Game/scripts/vscripts/addon_game_mode.lua
@@ -1,10 +1,25 @@
 -- Generated from template
 
-if CAddonTemplateGameMode == nil then
-	CAddonTemplateGameMode = class({})
+_G.nCOUNTDOWNTIMER = 901
+
+if CMWGameMode == nil then
+	_G.CMWGameMode = class({})
 end
 
+---------------------------------------------------------------------------
+-- Required .lua files
+---------------------------------------------------------------------------
+require( "events" )
+--require( "items" )
+require( "utility_functions" )
+
+
+---------------------------------------------------------------------------
+-- Precache
+---------------------------------------------------------------------------
 function Precache( context )
+	print("Precache( context )")
+
 	--[[
 		Precache things we know we'll use.  Possible file types include (but not limited to):
 			PrecacheResource( "model", "*.vmdl", context )
@@ -16,21 +31,316 @@ end
 
 -- Create the game mode when we activate
 function Activate()
-	GameRules.AddonTemplate = CAddonTemplateGameMode()
-	GameRules.AddonTemplate:InitGameMode()
+	print("Activate")
+	
+	GameRules.MW = CMWGameMode()
+	GameRules.MW:InitGameMode()
 end
 
-function CAddonTemplateGameMode:InitGameMode()
-	print( "Template addon is loaded." )
-	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
-end
 
--- Evaluate the state of the game
-function CAddonTemplateGameMode:OnThink()
-	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		--print( "Template addon script is running." )
-	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
-		return nil
+
+---------------------------------------------------------------------------
+-- Initializer
+---------------------------------------------------------------------------
+function CMWGameMode:InitGameMode()
+	print("CMWGameMode:InitGameMode()")
+	
+	print( "Mirana Wars Reborn is loaded." )
+	
+	self.m_TeamColors = {}
+	self.m_TeamColors[DOTA_TEAM_GOODGUYS] = { 61, 210, 150 }	--		Teal
+	self.m_TeamColors[DOTA_TEAM_BADGUYS]  = { 243, 201, 9 }		--		Yellow
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_1] = { 197, 77, 168 }	--      Pink
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_2] = { 255, 108, 0 }		--		Orange
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_3] = { 52, 85, 255 }		--		Blue
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_4] = { 101, 212, 19 }	--		Green
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_5] = { 129, 83, 54 }		--		Brown
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_6] = { 27, 192, 216 }	--		Cyan
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_7] = { 199, 228, 13 }	--		Olive
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_8] = { 140, 42, 244 }	--		Purple
+	
+	for team = 0, (DOTA_TEAM_COUNT-1) do
+		color = self.m_TeamColors[ team ]
+		if color then
+			SetTeamCustomHealthbarColor( team, color[1], color[2], color[3] )
+		end
 	end
+
+	self.m_VictoryMessages = {}
+	self.m_VictoryMessages[DOTA_TEAM_GOODGUYS] = "#VictoryMessage_GoodGuys"
+	self.m_VictoryMessages[DOTA_TEAM_BADGUYS]  = "#VictoryMessage_BadGuys"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_1] = "#VictoryMessage_Custom1"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_2] = "#VictoryMessage_Custom2"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_3] = "#VictoryMessage_Custom3"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_4] = "#VictoryMessage_Custom4"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_5] = "#VictoryMessage_Custom5"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_6] = "#VictoryMessage_Custom6"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_7] = "#VictoryMessage_Custom7"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_8] = "#VictoryMessage_Custom8"
+	
+	
+	self.m_GatheredShuffledTeams = {}
+	--self.numSpawnCamps = 5				-
+	--self.specialItem = ""					-
+	--self.spawnTime = 120					-
+	--self.nNextSpawnItemNumber = 1			-
+	--self.hasWarnedSpawn = false			-
+	--self.allSpawned = false				-
+	self.leadingTeam = -1
+	self.runnerupTeam = -1
+	self.leadingTeamScore = 0
+	self.runnerupTeamScore = 0
+	self.isGameTied = true
+	self.countdownEnabled = false
+	
+	
+	self.TEAM_KILLS_TO_WIN = 30
+	self.CLOSE_TO_VICTORY_THRESHOLD = 5
+	
+	
+	--------------------------------------------
+	self:GatherAndRegisterValidTeams()
+	--------------------------------------------
+	
+	
+	
+	GameRules:SetCustomGameEndDelay( 0 )
+	GameRules:SetCustomVictoryMessageDuration( 10 )
+	GameRules:SetPreGameTime( 10 )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesOverride( true )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesVisible( false )
+	GameRules:SetHideKillMessageHeaders( true )
+	GameRules:SetUseUniversalShopMode( true )
+	GameRules:GetGameModeEntity():SetLoseGoldOnDeath( false )
+	
+	----------------------------------------------------
+	--Couple of Listeners
+	
+	ListenToGameEvent( "game_rules_state_change", Dynamic_Wrap( CMWGameMode, 'OnGameRulesStateChange' ), self )
+	ListenToGameEvent( "npc_spawned", Dynamic_Wrap( CMWGameMode, "OnNPCSpawned" ), self )
+	ListenToGameEvent( "dota_team_kill_credit", Dynamic_Wrap( CMWGameMode, 'OnTeamKillCredit' ), self )
+	ListenToGameEvent( "entity_killed", Dynamic_Wrap( CMWGameMode, 'OnEntityKilled' ), self )
+	
+	
+	Convars:RegisterCommand( "overthrow_set_timer", function(...) return SetTimer( ... ) end, "Set the timer.", FCVAR_CHEAT )
+	Convars:RegisterCommand( "overthrow_force_end_game", function(...) return self:EndGame( DOTA_TEAM_GOODGUYS ) end, "Force the game to end.", FCVAR_CHEAT )
+	
+	
+	
+	
+	----------------------------------------------------
+	
+	
+	
+	GameRules:GetGameModeEntity():SetThink( "OnThink", self, 1 ) 
+	
+	--GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
+end
+
+
+
+
+---------------------------------------------------------------------------
+---------------------------------------------------------------------------
+function CMWGameMode:EndGame( victoryTeam )
+	print("CMWGameMode:EndGame( victoryTeam )")
+	
+	--[[
+		local overBoss = Entities:FindByName( nil, "@overboss" )
+		if overBoss then
+			local celebrate = overBoss:FindAbilityByName( 'dota_ability_celebrate' )
+			if celebrate then
+				overBoss:CastAbilityNoTarget( celebrate, -1 )
+			end
+		end
+	]]--
+
+	GameRules:SetGameWinner( victoryTeam )
+end
+
+
+---------------------------------------------------------------------------
+-- Get the color associated with a given teamID
+---------------------------------------------------------------------------
+function CMWGameMode:ColorForTeam( teamID )
+	print("CMWGameMode:ColorForTeam( teamID )")
+	
+	local color = self.m_TeamColors[ teamID ]
+	if color == nil then
+		color = { 255, 255, 255 } -- default to white
+	end
+	return color
+end
+
+
+---------------------------------------------------------------------------
+-- Put a label over a player's hero so people know who is on what team
+---------------------------------------------------------------------------
+function CMWGameMode:UpdatePlayerColor( nPlayerID )
+	print("CMWGameMode:UpdatePlayerColor( nPlayerID )")
+	
+	if not PlayerResource:HasSelectedHero( nPlayerID ) then
+		return
+	end
+
+	local hero = PlayerResource:GetSelectedHeroEntity( nPlayerID )
+	if hero == nil then
+		return
+	end
+
+	local teamID = PlayerResource:GetTeam( nPlayerID )
+	local color = self:ColorForTeam( teamID )
+	PlayerResource:SetCustomPlayerColor( nPlayerID, color[1], color[2], color[3] )
+end
+
+
+---------------------------------------------------------------------------
+-- Simple scoreboard using debug text
+---------------------------------------------------------------------------
+function CMWGameMode:UpdateScoreboard()
+	print("CMWGameMode:UpdateScoreboard()")
+	
+	local sortedTeams = {}
+	for _, team in pairs( self.m_GatheredShuffledTeams ) do
+		table.insert( sortedTeams, { teamID = team, teamScore = GetTeamHeroKills( team ) } )
+	end
+
+	-- reverse-sort by score
+	table.sort( sortedTeams, function(a,b) return ( a.teamScore > b.teamScore ) end )
+
+	for _, t in pairs( sortedTeams ) do
+		local clr = self:ColorForTeam( t.teamID )
+
+		-- Scaleform UI Scoreboard
+		local score = 
+		{
+			team_id = t.teamID,
+			team_score = t.teamScore
+		}
+		FireGameEvent( "score_board", score )
+	end
+	-- Leader effects (moved from OnTeamKillCredit)
+	local leader = sortedTeams[1].teamID
+	--print("Leader = " .. leader)
+	self.leadingTeam = leader
+	self.runnerupTeam = sortedTeams[2].teamID
+	self.leadingTeamScore = sortedTeams[1].teamScore
+	self.runnerupTeamScore = sortedTeams[2].teamScore
+	if sortedTeams[1].teamScore == sortedTeams[2].teamScore then
+		self.isGameTied = true
+	else
+		self.isGameTied = false
+	end
+	local allHeroes = HeroList:GetAllHeroes()
+	for _,entity in pairs( allHeroes) do
+		if entity:GetTeamNumber() == leader and sortedTeams[1].teamScore ~= sortedTeams[2].teamScore then
+			if entity:IsAlive() == true then
+				-- Attaching a particle to the leading team heroes
+				local existingParticle = entity:Attribute_GetIntValue( "particleID", -1 )
+       			if existingParticle == -1 then
+       				local particleLeader = ParticleManager:CreateParticle( "particles/leader/leader_overhead.vpcf", PATTACH_OVERHEAD_FOLLOW, entity )
+					ParticleManager:SetParticleControlEnt( particleLeader, PATTACH_OVERHEAD_FOLLOW, entity, PATTACH_OVERHEAD_FOLLOW, "follow_overhead", entity:GetAbsOrigin(), true )
+					entity:Attribute_SetIntValue( "particleID", particleLeader )
+				end
+			else
+				local particleLeader = entity:Attribute_GetIntValue( "particleID", -1 )
+				if particleLeader ~= -1 then
+					ParticleManager:DestroyParticle( particleLeader, true )
+					entity:DeleteAttribute( "particleID" )
+				end
+			end
+		else
+			local particleLeader = entity:Attribute_GetIntValue( "particleID", -1 )
+			if particleLeader ~= -1 then
+				ParticleManager:DestroyParticle( particleLeader, true )
+				entity:DeleteAttribute( "particleID" )
+			end
+		end
+	end
+end
+
+
+
+function CMWGameMode:OnThink()
+	print("CMWGameMode:OnThink()")
+	
+	for nPlayerID = 0, (DOTA_MAX_TEAM_PLAYERS-1) do
+		self:UpdatePlayerColor( nPlayerID )
+	end
+	
+	self:UpdateScoreboard()
+	-- Stop thinking if game is paused
+	if GameRules:IsGamePaused() == true then
+        return 1
+    end
+
+	if self.countdownEnabled == true then
+		CountdownTimer()
+		if nCOUNTDOWNTIMER == 30 then
+			CustomGameEventManager:Send_ServerToAllClients( "timer_alert", {} )
+		end
+		if nCOUNTDOWNTIMER <= 0 then
+			--Check to see if there's a tie
+			if self.isGameTied == false then
+				GameRules:SetCustomVictoryMessage( self.m_VictoryMessages[self.leadingTeam] )
+				CMWGameMode:EndGame( self.leadingTeam )
+				self.countdownEnabled = false
+			else
+				self.TEAM_KILLS_TO_WIN = self.leadingTeamScore + 1
+				local broadcast_killcount = 
+				{
+					killcount = self.TEAM_KILLS_TO_WIN
+				}
+				CustomGameEventManager:Send_ServerToAllClients( "overtime_alert", broadcast_killcount )
+			end
+       	end
+	end
+
 	return 1
+end
+
+---------------------------------------------------------------------------
+-- Scan the map to see which teams have spawn points
+---------------------------------------------------------------------------
+function CMWGameMode:GatherAndRegisterValidTeams()
+--	print( "GatherValidTeams:" )
+
+	local foundTeams = {}
+	for _, playerStart in pairs( Entities:FindAllByClassname( "info_player_start_dota" ) ) do
+		foundTeams[  playerStart:GetTeam() ] = true
+	end
+
+	local numTeams = TableCount(foundTeams)
+	print( "GatherValidTeams - Found spawns for a total of " .. numTeams .. " teams" )
+	
+	local foundTeamsList = {}
+	for t, _ in pairs( foundTeams ) do
+		table.insert( foundTeamsList, t )
+	end
+
+	if numTeams == 0 then
+		print( "GatherValidTeams - NO team spawns detected, defaulting to GOOD/BAD" )
+		table.insert( foundTeamsList, DOTA_TEAM_GOODGUYS )
+		table.insert( foundTeamsList, DOTA_TEAM_BADGUYS )
+		numTeams = 2
+	end
+
+	local maxPlayersPerValidTeam = math.floor( 10 / numTeams )
+
+	self.m_GatheredShuffledTeams = ShuffledList( foundTeamsList )
+
+	print( "Final shuffled team list:" )
+	for _, team in pairs( self.m_GatheredShuffledTeams ) do
+		print( " - " .. team .. " ( " .. GetTeamName( team ) .. " )" )
+	end
+
+	print( "Setting up teams:" )
+	for team = 0, (DOTA_TEAM_COUNT-1) do
+		local maxPlayers = 0
+		if ( nil ~= TableFindKey( foundTeamsList, team ) ) then
+			maxPlayers = maxPlayersPerValidTeam
+		end
+		print( " - " .. team .. " ( " .. GetTeamName( team ) .. " ) -> max players = " .. tostring(maxPlayers) )
+		GameRules:SetCustomGameTeamMaxPlayers( team, maxPlayers )
+	end
 end
---
+++ Game/scripts/vscripts/events.lua
@@ -0,0 +1,173 @@
+--[[ events.lua ]]
+
+---------------------------------------------------------------------------
+-- Event: Game state change handler
+---------------------------------------------------------------------------
+function CMWGameMode:OnGameRulesStateChange()
+	print("CMWGameMode:OnGameRulesStateChange()")
+	
+	local nNewState = GameRules:State_Get()
+	--print( "OnGameRulesStateChange: " .. nNewState )
+
+	if nNewState == DOTA_GAMERULES_STATE_HERO_SELECTION then
+
+	end
+
+	if nNewState == DOTA_GAMERULES_STATE_PRE_GAME then
+		local numberOfPlayers = PlayerResource:GetPlayerCount()
+		if numberOfPlayers > 7 then
+			self.TEAM_KILLS_TO_WIN = 30
+			nCOUNTDOWNTIMER = 901
+		elseif numberOfPlayers > 4 and numberOfPlayers <= 7 then
+			self.TEAM_KILLS_TO_WIN = 25
+			nCOUNTDOWNTIMER = 721
+		else
+			self.TEAM_KILLS_TO_WIN = 20
+			nCOUNTDOWNTIMER = 601
+		end
+		--print( "Kills to win = " .. tostring(self.TEAM_KILLS_TO_WIN) )
+
+		CustomNetTables:SetTableValue( "game_state", "victory_condition", { kills_to_win = self.TEAM_KILLS_TO_WIN } );
+
+		self._fPreGameStartTime = GameRules:GetGameTime()
+	end
+
+	if nNewState == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		--print( "OnGameRulesStateChange: Game In Progress" )
+		self.countdownEnabled = true
+		CustomGameEventManager:Send_ServerToAllClients( "show_timer", {} )
+		DoEntFire( "center_experience_ring_particles", "Start", "0", 0, self, self  )
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Event: OnNPCSpawned
+--------------------------------------------------------------------------------
+function CMWGameMode:OnNPCSpawned( event )
+	print("CMWGameMode:OnNPCSpawned( event )")
+	
+	local spawnedUnit = EntIndexToHScript( event.entindex )
+	if spawnedUnit:IsRealHero() then
+		-- Destroys the last hit effects
+		local deathEffects = spawnedUnit:Attribute_GetIntValue( "effectsID", -1 )
+		if deathEffects ~= -1 then
+			ParticleManager:DestroyParticle( deathEffects, true )
+			spawnedUnit:DeleteAttribute( "effectsID" )
+		end
+		if self.allSpawned == false then
+			if GetMapName() == "mines_trio" then
+				--print("mines_trio is the map")
+				--print("self.allSpawned is " .. tostring(self.allSpawned) )
+				local unitTeam = spawnedUnit:GetTeam()
+				local particleSpawn = ParticleManager:CreateParticleForTeam( "particles/addons_gameplay/player_deferred_light.vpcf", PATTACH_ABSORIGIN, spawnedUnit, unitTeam )
+				ParticleManager:SetParticleControlEnt( particleSpawn, PATTACH_ABSORIGIN, spawnedUnit, PATTACH_ABSORIGIN, "attach_origin", spawnedUnit:GetAbsOrigin(), true )
+			end
+		end
+	end
+end
+
+---------------------------------------------------------------------------
+-- Event: OnTeamKillCredit, see if anyone won
+---------------------------------------------------------------------------
+function CMWGameMode:OnTeamKillCredit( event )
+	print("CMWGameMode:OnTeamKillCredit( event )")
+	
+--	print( "OnKillCredit" )
+--	DeepPrint( event )
+
+	local nKillerID = event.killer_userid
+	local nTeamID = event.teamnumber
+	local nTeamKills = event.herokills
+	local nKillsRemaining = self.TEAM_KILLS_TO_WIN - nTeamKills
+	
+	local broadcast_kill_event =
+	{
+		killer_id = event.killer_userid,
+		team_id = event.teamnumber,
+		team_kills = nTeamKills,
+		kills_remaining = nKillsRemaining,
+		victory = 0,
+		close_to_victory = 0,
+		very_close_to_victory = 0,
+	}
+
+	if nKillsRemaining <= 0 then
+		GameRules:SetCustomVictoryMessage( self.m_VictoryMessages[nTeamID] )
+		GameRules:SetGameWinner( nTeamID )
+		broadcast_kill_event.victory = 1
+	elseif nKillsRemaining == 1 then
+		EmitGlobalSound( "ui.npe_objective_complete" )
+		broadcast_kill_event.very_close_to_victory = 1
+	elseif nKillsRemaining <= self.CLOSE_TO_VICTORY_THRESHOLD then
+		EmitGlobalSound( "ui.npe_objective_given" )
+		broadcast_kill_event.close_to_victory = 1
+	end
+
+	CustomGameEventManager:Send_ServerToAllClients( "kill_event", broadcast_kill_event )
+end
+
+---------------------------------------------------------------------------
+-- Event: OnEntityKilled
+---------------------------------------------------------------------------
+function CMWGameMode:OnEntityKilled( event )
+	print("CMWGameMode:OnEntityKilled( event )")
+	
+	local killedUnit = EntIndexToHScript( event.entindex_killed )
+	local killedTeam = killedUnit:GetTeam()
+	local hero = EntIndexToHScript( event.entindex_attacker )
+	local heroTeam = hero:GetTeam()
+	if killedUnit:IsRealHero() then
+		self.allSpawned = true
+		--print("Hero has been killed")
+		if hero:IsRealHero() and heroTeam ~= killedTeam then
+			--print("Granting killer xp")
+			if killedUnit:GetTeam() == self.leadingTeam and self.isGameTied == false then
+				local memberID = hero:GetPlayerID()
+				PlayerResource:ModifyGold( memberID, 500, true, 0 )
+				hero:AddExperience( 100, 0, false, false )
+				local name = hero:GetClassname()
+				local victim = killedUnit:GetClassname()
+				local kill_alert =
+					{
+						hero_id = hero:GetClassname()
+					}
+				CustomGameEventManager:Send_ServerToAllClients( "kill_alert", kill_alert )
+			else
+				hero:AddExperience( 50, 0, false, false )
+			end
+		end
+		--Granting XP to all heroes who assisted
+		local allHeroes = HeroList:GetAllHeroes()
+		for _,attacker in pairs( allHeroes ) do
+			--print(killedUnit:GetNumAttackers())
+			for i = 0, killedUnit:GetNumAttackers() - 1 do
+				if attacker == killedUnit:GetAttacker( i ) then
+					--print("Granting assist xp")
+					attacker:AddExperience( 25, 0, false, false )
+				end
+			end
+		end
+		if killedUnit:GetRespawnTime() > 10 then
+			--print("Hero has long respawn time")
+			if killedUnit:IsReincarnating() == true then
+				--print("Set time for Wraith King respawn disabled")
+				return nil
+			else
+				CMWGameMode:SetRespawnTime( killedTeam, killedUnit )
+			end
+		else
+			CMWGameMode:SetRespawnTime( killedTeam, killedUnit )
+		end
+	end
+end
+
+function CMWGameMode:SetRespawnTime( killedTeam, killedUnit )
+	print("CMWGameMode:SetRespawnTime( killedTeam, killedUnit )")
+	
+	--print("Setting time for respawn")
+	if killedTeam == self.leadingTeam and self.isGameTied == false then
+		killedUnit:SetTimeUntilRespawn(20)
+	else
+		killedUnit:SetTimeUntilRespawn(10)
+	end
+end
