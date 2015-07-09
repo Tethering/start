110d05ccce356ee5bbabd75a40e955be50715d20
imNavi <procenkoeg@mail.ru>
2015-07-09 07:57:07 +0300

    Hud is fine

--- Game/scripts/vscripts/addon_game_mode.lua
+++ Game/scripts/vscripts/addon_game_mode.lua
@@ -112,6 +112,7 @@ function CMWGameMode:InitGameMode()
 	GameRules:SetHideKillMessageHeaders( true )
 	GameRules:SetUseUniversalShopMode( true )
 	GameRules:GetGameModeEntity():SetLoseGoldOnDeath( false )
+	GameRules:GetGameModeEntity():SetBuybackEnabled( false )
 	
 	----------------------------------------------------
 	--Couple of Listeners
